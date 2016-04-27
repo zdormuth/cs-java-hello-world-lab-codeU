0ed35bc1c31a8868fd9b53087832609268816e1d
Zoe D <zdormuth@gmail.com>
2016-04-27 12:20:49 -0500

    Done

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,15 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+	String version = System.getProperty("java.version"); // returns 1.7.0_76 as a string
+	int position = version.indexOf('.'); //finds first occurance of . --> position = 1
+	position = version.indexOf('.', position + 1); // finds second occurance of . --> position = 3
+	return Double.parseDouble(version.substring(0, position));
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	HelloWorld v = new HelloWorld();
+	Double version_type = v.getVersion();
+	System.out.println(version_type);
     }
 }
